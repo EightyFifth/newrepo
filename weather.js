85e0ebd45eac1c7109799e7593ae61d47b5a857f
Sergey <52751920+EightyFifth@users.noreply.github.com>
2019-09-24 18:00:13 +0300

    minor

--- weather.js
+++ weather.js
@@ -16,17 +16,3 @@ function(data){
   $(".city").append(city);
   $(".weather").append(weather);
 });
-
-
-//document.getElementByClass('temp').innerHTML = temp;
-
-/*var request = new XMLHttpRequest();
-var url = "http://api.openweathermap.org/data/2.5/weather?q=Tallinn,ee&units=metric&APPID=0b8cbee81d0ead6a4cb183e0b7437fe0";
-
-request.open('GET', url, false);
-request.send();
-
-var response = request.response;
-var data = JSON.parse(response);
-
-console.log(data["text"]);*/
